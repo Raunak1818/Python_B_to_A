f9f4f43d8a228d4c10794d9fe510b12e6486be07
Raunak Raj Jaiswal <raunakrajjaiswal.com>
2026-06-07 15:56:13 +0530

    Add Day 9

---
+++ Day_9/Practise.py
@@ -0,0 +1,175 @@
+# i = 1
+
+# while i <= 10:
+#     print(i)
+#     i += 1
+    
+# print("time out!")
+
+
+# import time
+
+# i = 5
+
+# while i >= 1:
+#     print(i)
+#     time.sleep(1)
+#     i -= 1
+
+    
+# print("time's up")
+
+
+
+
+
+
+# num = int(input("Enter the number: "))
+
+# while num > 0:
+#     print(num)
+#     num -= 1
+
+# print("blast off")
+
+
+
+# import time
+
+# num = int(input("Enter the number: "))
+
+# for x in range(0, num):
+#     print(x)
+#     time.sleep(1)
+
+# print("finished")
+
+
+
+# import time
+
+# num = int(input("Enter the number: "))
+
+# for x in reversed(range(1 , num)):
+#     print(x)
+#     time.sleep(1)
+
+
+# print("finished")
+
+
+
+
+
+
+# import time
+
+# num = int(input("Enter the number: "))
+
+# for x in range(num, 0, -1):
+#     print(x)
+#     time.sleep(1)
+
+
+# print("finished")
+
+
+
+
+# import time
+
+# num = int(input("Enter a number: "))
+
+# while num > 0:
+#     print(num)
+#     num -= 1
+#     time.sleep(1)
+
+# print("finished")
+
+
+
+
+# num = int(input("Enter the number: "))
+
+# i = 1
+
+# while i <= num:
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
+
+
+
+
+# import time
+
+# num = int(input("Enter the number: "))
+
+# i = 1
+
+# while i <= num:
+#     if i % 2 == 0:
+#         print(i)
+#         time.sleep(1)
+#     i += 1
+        
+# print("above no. are even")
+
+
+
+# import time
+
+# num = int(input("enter the no.: "))
+
+# i = 2
+
+# while i <= num:
+#     print(i , end=" ")
+#     time.sleep(1)
+#     i +=2
+
+# print("above no. are even")
+
+
+
+
+
+# import time
+
+
+# num = int(input("enter value: "))
+
+# i = 1
+
+# while i <= num:
+#     if i % 2 != 0:
+#         print(i , end=" ")
+#         time.sleep(1)
+#     i += 1
+# print()
+
+
+# print("above no. are odd")
+
+
+
+
+
+
+# import time
+
+
+# num = int(input("enter value: "))
+
+# i = 1
+# sum = 0
+
+# while i <= num:
+#     # print(f"i: {i}")
+#     sum += i
+#     print(f"i: {i}, sum: {sum}")
+#     time.sleep(1)
+#     i += 1
+
+# print(f"total sum: {sum}" )
