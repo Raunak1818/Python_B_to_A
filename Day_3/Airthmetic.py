ba69925e9c0481f21d6f9147ff001df44e1b2bf4
Raunak Raj Jaiswal <raunakrajjaiswal.com>
2026-06-07 15:53:11 +0530

    Add Day 3

---
+++ Day_3/Airthmetic.py
@@ -0,0 +1,47 @@
+# friends = 5
+
+# friends = friends + 1
+# friends+= 1
+# friends = friends -2
+# friends -= 1
+# friends = friends * 1
+# friends *= 1
+# friends = friends / 1
+# friends /= 1
+# friends = friends ** 1
+# friends **= 1
+# remainder = friends % 2
+
+# print(friends)
+# print(remainder)
+
+
+
+
+
+# x = 3.14
+# y = 4
+# z = 5
+
+# # result = round(x)
+# # result = abs(y)
+# # result = pow(4,3)
+# # result = max(x,y,z)
+# result = min(x,y,z)
+
+# print(result)
+
+
+
+
+# import math 
+
+# x = 9.9
+
+# # print(math.pi)
+# # print(math.e)
+# # result = math.sqrt(x)
+# result = math.ceil(x)
+# result = math.floor(x)
+# print(result)
+
---
+++ Day_3/Practise.py
@@ -0,0 +1,43 @@
+# # Q1. Claculate the circumference
+# #       (circumference = 2*pie*r)
+
+# import math
+
+# radius = float(input(f"Enter the radius of circe: "))
+
+# circumference = 2 * math.pi * radius
+
+# # print(f"The circumference is: {circumference}")
+# print(f"The circumference is: {round(circumference, 2)}cm")
+
+
+
+
+# # Q2. Calculate the Area of circle
+# #     (A = pie * (r)squre)
+
+# import math 
+
+# radius = float(input(f"Enter the radius of circle: "))
+
+# area = math.pi * pow(radius, 2)
+
+# print(f"Area of circle: {round(area, 2)}cm")
+
+
+
+
+#Q3. calulate the hypotunus of right angle triangle
+
+# import math 
+
+# a = float(input("Enter the value of A: "))
+# b = float(input("Enter the value of B: "))
+
+# c = math.sqrt(pow(a, 2) + pow(b , 2))
+
+# print(f"The value of hypotunus of right angle triangle: {c}")
+
+
+
+
