b5642c46b6251e919e26e6f8c8ac38aa521fd554
Raunak Raj Jaiswal <raunakrajjaiswal.com>
2026-06-07 15:55:04 +0530

    Add Day 6

---
+++ Day_6/Practise.py
@@ -0,0 +1,149 @@
+# Q1 - Add two number
+
+# a = int(input("Enter the first number: "))
+# b  = int(input("Enter the second number: "))
+
+# sum = a + b
+# print(f"Sum: {sum}")
+
+
+
+# # Q2 - Check even or odd
+
+# num = int(input("Enter the number: "))
+
+# if num % 2 == 0:
+#     print(f"{num} is even")
+# else:
+#     print(f"{num} is odd")
+
+
+# Q3 - Find largest number
+
+# num = 5,6,7
+# max_num = max(num)
+
+# print(f"{max_num} is largest")
+
+
+# # Q4. Reverse a letter
+
+# name = "hello"
+
+# name = name[::-1]
+# print(name)
+
+
+
+# Q5 - Check Palindrome
+
+# letter = input("Enter the letter: ")
+
+# if letter == letter[::-1]:
+#     print(f"{letter} is palindrome")
+# else:
+#     print(f"{letter} is not palindrome")
+
+
+
+# Q6 - print number from 1 to 10
+
+# i = 1
+
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+
+
+# # Q7 - print number from 10 to 1
+
+# i = 10
+
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+
+
+# # Q7 - print even number from 1 to 20
+
+i = 1
+
+# while i <= 20:
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
+    
+
+
+#  Q7 - print odd number from 1 to 20
+
+# i = 1
+
+# while i <= 20:
+#     if i % 2 != 0:
+#         print(i)
+#     i += 1
+
+
+
+
+# Q8 - Sum of numbers from 1 to 10
+
+# i = 1
+# sum = 0
+
+# while i <= 10:
+#     sum += i
+#     i += 1
+
+# print(f"sum: {sum}")
+
+
+
+
+
+
+# 9 - coversation b/w two people
+
+# person1 =""
+# person2 = ""
+
+# while person1 != "bye" and person2 != "bye":
+
+#     person1 = input("person1: ")
+
+#     if person1 =="bye":
+#         print("person2: Goodbye")
+#         break
+
+#     person2 = input("person2: ")
+
+#     if person2 =="bye":
+#         print("person1: Goodbye")
+#         break
+
+
+
+# 10 - chat with bot 
+
+# user = ""
+
+# while user != "bye":
+#     user = input("you: ")
+
+#     if user == "hello":
+#         print("Bot: hello! i was sleeping in the computer")
+    
+#     elif user == "how are you":
+#         print("Bot: i am fine but my wifi is emotionally unstable")
+
+#     elif user == "what is your name":
+#         print("Bot: my name is RJPY")
+
+#     elif user == ("bye"):
+#         print("Bot: Bye, Don't forget to charge my brain battery")
+
+#     else:
+#         print("Bot: I don't understand, but I am pretending I do.")
